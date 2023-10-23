2627109fa6cb8e7327a07994200a2431b7c32a24
Jupiteries <128291249+Jupiteries@users.noreply.github.com>
2023-10-23 14:31:46 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace GroomingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Owner owner = new Owner();
+            GroomingService.Welcome(owner);
+            GroomingService.GroomerAvailabilityChecker(owner);
+        }
+    }
+}
