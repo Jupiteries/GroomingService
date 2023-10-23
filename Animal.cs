1653ec20261b38d2eef50b0ddd73fa623414fae2
Jupiteries <128291249+Jupiteries@users.noreply.github.com>
2023-10-23 14:29:15 +0400

    Create Animal.cs

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.Threading.Tasks;
+
+namespace GroomingService
+{
+    public class Animal
+    {
+        public string? Name { get; set; }
+        public string? Breed { get; set; }
+        public string? Type { get; set; }
+        public int Id { get; set; }
+    }
+}
