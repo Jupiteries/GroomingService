00c06a3ac0f2c76285a606e8cee6ebefe5e00ea1
Jupiteries <128291249+Jupiteries@users.noreply.github.com>
2023-10-23 14:29:40 +0400

    Create Groomer.cs

---
+++ Groomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroomingService
+{
+    class Groomer
+    {
+        public int ID { get; set; }
+        public static string Name = "John";
+        public static string Brush = "A good one";
+        public static string Shower = "A relaxing one";
+        public static bool isFree = true;
+    }
+}
